559c061e7a63abc2c8406e22e7ff23b4f87ed232
afganirw07 <afganirwansyah070708@gmail.com>
2026-02-12 19:58:06 +0700

    feat: setup database using prisma

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World with Bun and Express!");
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Hello World with Bun and Express!" });
 });
 
 app.listen(port, () => {
