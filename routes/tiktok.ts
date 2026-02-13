c36b67ddea2ab3ddd70e0fdd031c278b67d2adde
afganirw07 <afganirwansyah070708@gmail.com>
2026-02-13 14:43:39 +0700

    feat: Add JWT authentication middleware to TikTok and YouTube routes

--- routes/tiktok.ts
+++ routes/tiktok.ts
@@ -2,10 +2,11 @@ import { tiktokDownloader } from "../controllers/Tiktok";
 import { Router } from "express";
 import { TikTokSchema } from "../schemas/tiktok";
 import { validateData } from "../middleware/zod";
+import { JwtVerify } from "../middleware/jwt";
 
 const router = Router();
 
 // Tiktok
-router.post("/tiktok", validateData(TikTokSchema), tiktokDownloader);
+router.post("/tiktok", JwtVerify, validateData(TikTokSchema), tiktokDownloader);
 
 export default router;
--- routes/youtube.ts
+++ routes/youtube.ts
@@ -2,9 +2,10 @@ import { youtubeDownloader } from "../controllers/Youtube";
 import { Router } from "express";
 import { YoutubeSchema } from "../schemas/youtube";
 import { validateData } from "../middleware/zod";
+import { JwtVerify } from "../middleware/jwt";
 
 const router = Router();
 
-router.post("/youtube", validateData(YoutubeSchema), youtubeDownloader);
+router.post("/youtube", JwtVerify, validateData(YoutubeSchema), youtubeDownloader);
 
 export default router;
