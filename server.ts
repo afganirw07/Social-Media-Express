060133eb96152dfe5a0bbc35b20cdd765e1ea2b0
afganirw07 <afganirwansyah070708@gmail.com>
2026-02-18 23:30:09 +0700

    feat: add CORS middleware for cross-origin requests

--- server.ts
+++ server.ts
@@ -1,19 +1,26 @@
 import express from "express";
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 import User from "./routes/user.ts";
 import Tiktok from "./routes/tiktok.ts";
 import Youtube from "./routes/youtube.ts";
 import Facebook from "./routes/facebook.ts";
 import Instagram from "./routes/instagram.ts";
 import Twitter from "./routes/twitter.ts";
+import cors from "cors";
 
 
 // server setup
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT;
 
 app.use(express.json());
 
+app.use(cors({
+    origin: "http://localhost:3000", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 // routes
 app.get("/", (req: Request, res: Response) => {
