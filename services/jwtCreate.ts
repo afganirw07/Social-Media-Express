9e9e06653180b8c4b9104e18398fa673714e9737
afganirw07 <afganirwansyah070708@gmail.com>
2026-02-19 00:59:23 +0700

    feat: extend JWT token expiration to 1 day

--- services/jwtCreate.ts
+++ services/jwtCreate.ts
@@ -8,6 +8,6 @@ interface JwtPayload {
 
 export const jwtCreate = (payload: JwtPayload): string => {
     return jwt.sign(payload, process.env.JWT_SECRET as string, {
-        expiresIn: "1h",
+        expiresIn: "1d",
     });
 };
